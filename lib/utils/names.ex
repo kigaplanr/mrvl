9c38ab524269f1353bb7200a9598b312fc037ca4
Koni vKxni <eventbeate@gmail.com>
2022-12-17 14:38:39 +0100

    name string type

--- lib/utils/names.ex
+++ lib/utils/names.ex
@@ -1,11 +1,16 @@
 defmodule Mrvl.Names do
-
   @moduledoc """
   These names are for testing purposes only, make sure to use custom names later on.
 
   Just remember, this is a minimal viable product, so we are not going to worry about it here.
   Something to reproduce the big shit.
   """
+
+  @type nametype :: String.t()
+
+  @spec random_name() :: nametype
+  @spec random_lastname() :: nametype
+
   def random_name() do
     [
       "John",
